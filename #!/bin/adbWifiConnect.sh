7b140abbafda3c98947bd2f17cb82080f83700ac
gilador <gilador84@gmail.com>
2019-05-30 15:59:40 +0300

    Update adbWifiConnect.sh

--- #!/bin/adbWifiConnect.sh
+++ #!/bin/adbWifiConnect.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#connects
+#connects a device the to wifi
+#make sure you are connected to the same network!!
+echo "devices"
 adb devices
+echo "device ip"
 ip=$(adb shell ip route | awk '{print $9}')
 echo $ip
 echo "kill-server"
@@ -9,14 +12,15 @@ wait
 echo "start-server"
 adb start-server
 wait
+echo "reverse tcp:8081 tcp:8081" #needed for development only - in case you run a server such as the react-native packager 
+adb reverse tcp:8081 tcp:8081
+wait
 echo "tcpip 5556"
 adb tcpip 5555
-wait
-echo "reverse tcp:8081 tcp:8081"
-adb reverse tcp:8081 tcp:8081
 wait
 echo "connect ${ip}:5556"
 adb connect ${ip}:5555
 wait
 echo "devices:"
 adb devices
+
